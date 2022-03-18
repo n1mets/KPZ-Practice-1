fc11128462a4bd5a6c100d254aaceaf3db32af20
n1mets <99960812+n1mets@users.noreply.github.com>
2022-03-18 12:58:24 +0200

    Add files via upload

--- lab3-validator.py
+++ lab3-validator.py
@@ -6,6 +6,8 @@ def validate_ip_address(address):
         print("IP address {} працює".format(address, ip))
     except ValueError:
         print("IP address {} не працює".format(address)) 
-validate_ip_address("10.10.10.10")
-validate_ip_address("10.10.10.01")
+validate_ip_address("93.170.189.158")
+validate_ip_address("93.170.189.448")
+validate_ip_address("93.220.788.448")
+
 
