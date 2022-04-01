6d2540b0ce240598bd02cd7f58245e4e5fd6b526
n1mets <99960812+n1mets@users.noreply.github.com>
2022-04-01 12:23:04 +0300

    Add files via upload

--- lab3-validator.py
+++ lab3-validator.py
@@ -1,13 +1,41 @@
-import ipaddress 
+from ipaddress import ip_address, IPv4Address, IPv6Address, AddressValueError
 
-def validate_ip_address(address):
+
+def _is_valid_ip_address(ip, ipv_type: str = 'any') -> bool:
+    """Validates an ipd address"""
     try:
-        ip = ipaddress.ip_address(address)
-        print("IP address {} працює".format(address, ip))
-    except ValueError:
-        print("IP address {} не працює".format(address)) 
-validate_ip_address("93.170.189.158")
-validate_ip_address("93.170.189.448")
-validate_ip_address("93.220.788.448")
+        if ipv_type == 'any':
+            ip_address(ip)
+        elif ipv_type == 'ipv4':
+            IPv4Address(ip)
+        elif ipv_type == 'ipv6':
+            IPv6Address(ip)
+        else:
+            raise NotImplementedError
+    except (AddressValueError, ValueError):
+        return False
+    else:
+        return True
+
+def run_tests():
+    ipv4 = '192.168.0.1'
+    ipv6 = '2001:db8::1000'
+    bad = "I AM NOT AN IP"
+    is_pv4 = _is_valid_ip_address(ipv4)
+    is_pv6 = _is_valid_ip_address(ipv6)
+    bad_ip = _is_valid_ip_address(bad)
+
+    am_i_pv4 = _is_valid_ip_address(ipv6, ipv_type='ipv4')
+    am_i_pv6 = _is_valid_ip_address(ipv4, ipv_type='ipv6')
+    print(f'''
+    * is_pv4 -> {is_pv4}
+    * is_pv6 -> {is_pv6}
+    * bad_ip -> {bad_ip}
+    * am_i_pv4 -> {am_i_pv4}
+    * am_i_pv6 -> {am_i_pv6}
+    ''')
 
 
+
+if __name__ == '__main__':
+    run_tests()
